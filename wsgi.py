4c38828b20ada5523823c815082a06f39c35ca27
Aj800 <40776498+Aj800@users.noreply.github.com>
2019-07-03 22:42:43 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Adam!"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     application.run()
